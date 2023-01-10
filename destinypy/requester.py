c33b1cc9e784dcc13a00f704cbc185afbf01e604
qwhaty <williamknight06@hotmail.co.uk>
2023-01-10 13:53:14 +0000

    updated requester.py with new exceptions

--- destinypy/requester.py
+++ destinypy/requester.py
@@ -1,25 +1,14 @@
 from requests import request
-
-class APIError(Exception):
-
-    def __init__(self, message):
-        self.message = message
-        super().__init__(self.message)
+from destinypy import exceptions
 
 def GET(url: str, data={}, headers={}) -> dict:
     r = request("GET", url, data=data, headers=headers)
     if not r.ok:
-        if r.json()["ErrorStatus"] == "UserCannotFindRequestedUser":
-            raise LookupError(r.json()["Message"])
-        else:
-            raise APIError("Request failed")
+        raise exceptions.APIError(r.json()["ErrorCode"], r.json()["Message"])
     return r.json()
     
 def POST(url: str, data={}, headers={}) -> dict:
     r = request("POST", url, data=data, headers=headers)
     if not r.ok:
-        if r.json()["ErrorStatus"] == "UserCannotFindRequestedUser":
-            raise LookupError(r.json()["Message"])
-        else:
-            raise APIError("Request failed")
+        raise exceptions.APIError(r.json()["ErrorCode"], r.json()["Message"])
     return r.json()
